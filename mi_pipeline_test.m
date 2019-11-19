dbd4989a1e9a1de4f8c577ed0fe9f402a524ab44
neuRowsATL <bchung808@gmail.com>
2019-11-19 16:51:20 -0500

    added tests for mi_data_behavior.m

--- mi_pipeline_test.m
+++ mi_pipeline_test.m
@@ -19,6 +19,7 @@ load('TestData/20191018_bl21lb21_171218_spikes.mat');
 
 try
     disp([newline newline]);
+    clear d;
     d = mi_data('test');
     clear d;
 catch
@@ -28,6 +29,7 @@ end
 
 try
     disp([newline newline]);
+    clear d;
     d = mi_data('test', 'verbose', 5);
 
     str_unit1 = 'TestData/20191018_bl21lb21_171218_spikes.mat/unit1';
@@ -79,9 +81,92 @@ try
         errs = [errs newline 'Importing: data info'];
     end
 
+    if all(size(get_data(d,'unit1')) == size(unit1)) && ...
+            all(size(get_data(d,'unit2')) == size(unit2)) && ...
+            all(size(get_data(d,'unit3')) == size(unit3))
+        disp('Pulled: data');
+    else
+        errs = [errs newline 'Pulling: get_data() dims do not match'];
+    end
+    
     disp(errs);
 catch
     global_errs{end+1} = {'Instantiating mi_data with ID and verbose'};
+    % Not possible to proceed without mi_data class
+    
+    disp([newline newline '===== ===== ===== ===== =====' newline 'GLOBAL ERRORS' newline]);
+    for i=1:length(global_errs)
+        disp(global_errs{i});
+    end
+    disp(['----- ----- ----- ----- -----' newline]);
+    
+    error('FATAL ERROR: Unable to construct mi_data objects');
+end
+
+%%
+
+try
+    disp([newline newline]);
+    clear d;
+    d = mi_data_behavior('test');
+    clear d;
+catch
+    global_errs{end+1} = {'Intantiating mi_data_behavior with ID only'};
+    disp([newline 'ERROR: Unable to instantiate mi_data_behavior with ID only']);
+end
+
+try
+    disp([newline newline]);
+    clear d
+    d = mi_data_behavior('test', 'verbose', 5);
+
+    add_cycleTimes(d, [unit1' unit1'], str_unit1, 30000);
+
+    % CHECK OBJECT FOR INSTANTIATION CONSISTENCY
+    errs = ['----- ----- ----- ----- -----' newline 'ERRORS:' newline];
+    disp([newline newline '===== ===== ===== ===== ===== ' newline 'SUCCESSFUL:' newline]);
+    if strcmp(d.ID, 'test')
+        disp('Assigned: ID');
+    else
+        errs = [errs newline 'Assigning: ID'];
+    end
+    if d.Fs == 30000
+        disp('Assigned: Fs');
+    else
+        errs = [errs newline 'Assigning: Fs'];
+    end
+    if d.verbose == 5
+        disp('Assigned: verbose');
+    else
+        errs = [errs newline 'Assigning: verbose'];
+    end
+    if isfield(d.data, 'cycleTimes')
+        disp('assigned: cycleTimes');
+    else
+        errs = [errs newline 'Assigning: cycleTimes'];
+    end
+
+    if all(size(d.data.cycleTimes.data) == [size(unit1,2) 2])
+        disp('Imported: cycleTimes');
+    else
+        errs = [errs newline 'Importing: cycleTimes dims do not match'];
+    end
+
+    if strcmp(d.data.cycleTimes.info,str_unit1)
+        disp('Imported: info');
+    else
+        errs = [errs newline 'Importing: cycleTimes info'];
+    end
+
+    if all(size(get_cycleTimes(d)) == [size(unit1,2) 2])
+        disp('Pulled: cycleTimes');
+    else
+        errs = [errs newline 'Pulling: get_cycleTimes() dims do not match'];
+    end
+    
+    disp(errs);
+catch
+    global_errs{end+1} = {'Instantiating mi_data_behavior with ID and verbose'};
     % Not possible to proceed without mi_data class
     
     disp([newline newline '===== ===== ===== ===== =====' newline 'GLOBAL ERRORS' newline]);
