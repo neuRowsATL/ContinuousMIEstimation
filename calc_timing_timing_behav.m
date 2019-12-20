68d46a10218bd5948581d8c8798f63a9fc8f6094
Rachel Barker <Rachel@MacBook-Pro-8.local>
2019-12-20 16:39:53 -0500

    Constructor for timing_timing_behav

--- calc_timing_timing_behav.m
+++ calc_timing_timing_behav.m
@@ -1,4 +1,5 @@
 classdef calc_timing_timing_behav < mi_analysis
+    
     %Each of these objects sets the stage to calculate the mutual
     %information between spike timing of neuron 1 and spike timing of neuron 2 and stores the results of
     %the calculation. 
@@ -9,16 +10,90 @@ classdef calc_timing_timing_behav < mi_analysis
     % We will have to see how much data we have. 
     
     properties
+        n_timeBase
+        b_timeBase
+        feature
+        start
+        dur
+        nSamp
+        nPC
     end
     
     methods
-       function obj = calc_timing_timing_behav(objData,vars)
-            if length(vars) ~= 2
-                error('Expected two variables specified');
+       function obj = calc_timing_timing_behav(objData,objBehav, varNames, varargin)
+        % Set up input parser
+            p = inputParser;
+            
+            % Set required inputs
+            validate_objData = @(x) assert(isa(x, 'mi_data_neural'), 'objData must be a neural data subclass');
+            p.addRequired('objData', validate_objData);
+            
+            validate_objBehav = @(x) assert(isa(x, 'mi_data_behavior'), 'objBehav must be a behavioral data subclass');
+            p.addRequired('objBehav', validate_objBehav);
+            
+            validate_varNames = @(x) assert(iscell(x) && (length(x) == 2), 'varNames must be a cell of length 1');
+            p.addRequired('varNames', validate_varNames);
+                        
+            % Set parameters
+            default_n_timeBase = 'time';
+            valid_n_timeBases = {'time', 'phase'};
+            validate_n_timeBase = @(x) assert(ischar(x) && ismember(x, valid_n_timeBases), 'n_timeBase must be: time, phase');
+            p.addParameter('n_timeBase', default_n_timeBase, validate_n_timeBase); 
+
+                                    
+            % Set parameters
+            default_b_timeBase = 'phase';
+            valid_b_timeBases = {'time', 'phase'};
+            validate_b_timeBase = @(x) assert(ischar(x) && ismember(x, valid_timeBases), 'b_timeBase must be: time, phase');
+            p.addParameter('b_timeBase', default_b_timeBase, validate_b_timeBase); 
+
+            default_feature = 'residual';
+            valid_feature = {'raw', 'pca', 'residual'};
+            validate_feature = @(x) assert(ischar(x) && ismember(x, valid_feature), 'feature  must be: raw, pca, residual');
+            p.addParameter('feature', default_feature, validate_feature); 
+
+            default_start = pi/2;
+            validate_start = @(x) assert((0 < x < 2*pi) || isinteger(x), 'start must be in units of radians or ms, and must match b_timeBase');
+            p.addParameter('start', default_start, validate_start); 
+
+            default_dur = pi;
+            validate_dur = @(x) assert((0 < x < 2*pi) || isinteger(x), 'dur must be in units of radians or ms, and must match b_timeBase');
+            p.addParameter('dur', default_dur, validate_dur); 
+
+            default_nSamp = 11;
+            validate_nSamp = @(x) assert(isinteger(x), 'nSamp must be an integer');
+            p.addParameter('nSamp', default_nSamp, validate_nSamp);
+
+            default_nPC = 3;
+            validate_nPC = @(x) assert(isinteger(x), 'nPC must be an integer');
+            p.addParameter('nPC', default_nPC, validate_nPC);
+
+            % Prepare InputParser to parse only desired inputs
+            p.KeepUnmatched = 1;
+            p.parse(objData, objBehav, varNames, varargin{:});
+                                   
+            % Define validated inputs to parent constructor
+            objData = p.Results.objData;
+            objBehav = p.Results.objBehav;
+            varNames = p.Results.varNames;
+            
+            % One more validation: Check that varNames references valid fields of objData
+            for ivarNames = 1:length(varNames)
+                assert(isfield(objData.data , varNames{ivarNames}), ['varName: ' varNames{ivarNames} 'is not a valid field of the neural data object']); 
             end
 
-            obj@mi_analysis(objData, vars);
-
+                        
+            % Call parent constructor
+            obj@mi_analysis(objData, objBehav, varNames, varargin{:});
+            
+            % Define other properties of subclass object
+            obj.n_timeBase = p.Results.n_timeBase;
+            obj.b_timeBase = p.Results.b_timeBase;
+            obj.feature = p.Results.feature;
+            obj.start = p.Results.start;
+            obj.dur = p.Results.dur;
+            obj.nSamp = p.Results.nSamp;
+            obj.nPC = p.Results.nPC;
         end
         
         function buildMIs(obj)
